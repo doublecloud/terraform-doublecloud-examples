fccd0dfeaf0177bded6899d28b32063c91296f7a
Yuriy Natarov <acuion@yandex-team.ru>
2024-06-05 19:16:21 +0200

    Metrika2CH example

---
+++ metrika_to_ch/main.tf
@@ -0,0 +1,15 @@
+// Here is where we are defining
+// our Terraform settings
+terraform {
+  required_providers {
+    doublecloud = {
+      source  = "registry.terraform.io/doublecloud/doublecloud"
+      version = ">= 0.1.6"
+    }
+  }
+}
+
+provider "doublecloud" {
+  # See https://double.cloud/docs/en/public-api/tutorials/transfer-api-quickstart on how to obtain this file
+  authorized_key = file(var.dc_key_path)
+}

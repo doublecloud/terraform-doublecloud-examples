cd06031e25655f94994f9e4ae08588e80804fa13
MennaTullahTaha <menna.tullah.magdy731@gmail.com>
2024-08-29 14:49:13 +0200

    UPD: refactored variables.tf

--- dc_kinesis_clickhouse_transfer/variables.tf
+++ dc_kinesis_clickhouse_transfer/variables.tf
@@ -5,52 +5,55 @@ variable "stream_name" {
 }
 
 variable "aws_profile" {
-  default = "default"
+  description = "AWS Profile"
+  type        = string
+  default     = "default"
 }
 
 variable "federation_id" {
-  type        = string
   description = "Federation ID to auth"
+  type        = string
   default     = ""
 }
 
 variable "aws_region_id" {
-  type        = string
   description = "ID of the AWS region in which to create resources"
+  type        = string
   default     = "eu-central-1"
 }
 
 variable "project_id" {
-  type        = string
   description = "ID of the DoubleCloud project in which to create resources"
+  type        = string
+  default     = ""
 }
 
 variable "enable_transfer" {
+  description = "Create delivery from kafka to clickhouse via DoubleCloud.Transfer"
   type        = bool
   default     = false
-  description = "Create delivery from kafka to clickhouse via DoubleCloud.Transfer"
 }
 
 variable "aws_access_key_id" {
+  description = "AWS Secret Key with kinesis reader permissions"
   type        = string
   default     = ""
-  description = "AWS Secret Key with kinesis reader permissions"
 }
 
 variable "aws_secret_access_key" {
+  description = "AWS Secret Access Key with kinesis reader permissions"
   type        = string
   default     = ""
-  description = "AWS Secret Access Key with kinesis reader permissions"
 }
 
 variable "endpoint" {
+  description = "Doublecloud REST API endpoint"
   type        = string
   default     = ""
-  description = "Doublecloud REST API endpoint"
 }
 
 variable "federation_endpoint" {
+  description = "Doublecloud gRPC API endpoint"
   type        = string
   default     = ""
-  description = "Doublecloud gRPC API endpoint"
 }

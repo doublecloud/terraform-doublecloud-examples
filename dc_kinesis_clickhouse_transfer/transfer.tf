51d837f30d06120329b591d1bb69bcdeb64bf997
MennaTullahTaha <menna.tullah.magdy731@gmail.com>
2024-08-29 13:11:13 +0200

    UPD: create transfer endpoint in addition to kinesis source endpoint and clickhouse target endpoint

---
+++ dc_kinesis_clickhouse_transfer/transfer.tf
@@ -0,0 +1,99 @@
+resource "doublecloud_transfer_endpoint" "source" {
+  name       = "kinesis-source"
+  project_id = var.project_id
+  settings {
+    kinesis_source {
+      aws_access_key_id     = var.aws_access_key_id
+      aws_secret_access_key = var.aws_secret_access_key
+      region                = var.aws_region_id
+      stream_name           = var.stream_name
+      parser {
+        json {
+          schema {
+            fields {
+              field {
+                name     = "Topic"
+                type     = "string"
+                key      = true
+                required = true
+              }
+              field {
+                name     = "Partition"
+                type     = "uint32"
+                key      = true
+                required = true
+              }
+              field {
+                name     = "Offset"
+                type     = "uint32"
+                key      = true
+                required = true
+              }
+              field {
+                name     = "Timestamp"
+                type     = "datetime"
+                key      = false
+                required = false
+              }
+              field {
+                name     = "Headers"
+                type     = "string"
+                key      = false
+                required = false
+              }
+              field {
+                name     = "Value"
+                type     = "string"
+                key      = false
+                required = false
+              }
+              field {
+                name     = "Transfer_commit_time"
+                type     = "uint64"
+                key      = false
+                required = false
+              }
+              field {
+                name     = "Transfer_delete_time"
+                type     = "uint64"
+                key      = false
+                required = false
+              }
+            }
+          }
+          null_keys_allowed = false
+          add_rest_column   = true
+        }
+      }
+    }
+  }
+}
+
+resource "doublecloud_transfer_endpoint" "clickstream-target" {
+  count      = var.enable_transfer ? 1 : 0
+  name       = "clickstream-target"
+  project_id = var.project_id
+  settings {
+    clickhouse_target {
+      clickhouse_cleanup_policy = "DROP"
+      connection {
+        address {
+          cluster_id = doublecloud_clickhouse_cluster.target-clickhouse.id
+        }
+        database = "default"
+        user     = "admin"
+      }
+    }
+  }
+}
+
+resource "doublecloud_transfer" "clickstream-transfer" {
+  count      = var.enable_transfer ? 1 : 0
+  name       = "clickstream-transfer"
+  project_id = var.project_id
+  source     = doublecloud_transfer_endpoint.source.id
+  target     = doublecloud_transfer_endpoint.clickstream-target[count.index].id
+  type       = "INCREMENT_ONLY"
+  activated  = true
+}
+

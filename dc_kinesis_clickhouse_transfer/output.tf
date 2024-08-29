fed30f39f37d891d4c8f92f66b936a23f571055e
MennaTullahTaha <menna.tullah.magdy731@gmail.com>
2024-08-29 13:09:53 +0200

    UPD: create clickhouse_connection output

---
+++ dc_kinesis_clickhouse_transfer/output.tf
@@ -0,0 +1,12 @@
+// Grab newly create Clickhouse data
+data "doublecloud_clickhouse" "target-clickhouse" {
+  name       = doublecloud_clickhouse_cluster.target-clickhouse.name
+  project_id = var.project_id
+}
+
+// This will output the database endpoint
+output "clikchouse_connection" {
+  description = "Clickhouse Connection profile"
+  value       = data.doublecloud_clickhouse.target-clickhouse.connection_info
+  sensitive   = true
+}
